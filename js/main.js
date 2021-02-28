089fabbb4c348cee618ddbd23b45a6e434be8314
johced <johan.cederstrand@bahnhof.se>
2021-02-28 16:09:13 +0100

    cleaned my js hover function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 window.onload = function () {
-  // document.getElementById('changeBg').addEventListener('mouseover', changeColorBg);
-  // document.getElementById('changeBg').addEventListener('mouseout', changeColorBack);
+  document.getElementById('changeBg').addEventListener('mouseover', changeColorBg);
+  document.getElementById('changeBg').addEventListener('mouseout', changeColorBack);
 };
 
 function changeColorBg() {
-  // document.body.style.backgroundColor = '#392e37';
-  // document.body.style.backgroundImage =
-  //   'linear-gradient(45deg, #342e37, #342e37, #342e37, #736f72)';
   document.getElementById('gitBg').style.color = '#dce1e3';
   document.getElementById('linkedBg').style.color = '#dce1e3';
-  // document.getElementById('aboutmeBg').style.color = '#736f72';
-  // document.getElementById('highlightBg').style.color = '#736f72';
 }
 
 function changeColorBack() {
-  // document.body.style.backgroundImage = '';
   document.getElementById('gitBg').style.color = '';
   document.getElementById('linkedBg').style.color = '';
-  // document.getElementById('aboutmeBg').style.color = '';
-  // document.getElementById('highlightBg').style.color = '';
 }
