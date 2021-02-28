afca58043f1c4722739f0e9e86997bb22936a2b1
johced <johan.cederstrand@bahnhof.se>
2021-02-28 15:14:25 +0100

    added hover effect on contact button

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 window.onload = function () {
-  document.getElementById('changeBg').addEventListener('mouseover', changeColorBg);
-  document.getElementById('changeBg').addEventListener('mouseout', changeColorBack);
+  // document.getElementById('changeBg').addEventListener('mouseover', changeColorBg);
+  // document.getElementById('changeBg').addEventListener('mouseout', changeColorBack);
 };
 
 function changeColorBg() {
-  document.body.style.backgroundColor = '#372e37';
-
-  // document.getElementById('gitBg').style.color = '#736f72';
-  // document.getElementById('linkedBg').style.color = '#736f72';
+  // document.body.style.backgroundColor = '#392e37';
+  // document.body.style.backgroundImage =
+  //   'linear-gradient(45deg, #342e37, #342e37, #342e37, #736f72)';
+  document.getElementById('gitBg').style.color = '#dce1e3';
+  document.getElementById('linkedBg').style.color = '#dce1e3';
   // document.getElementById('aboutmeBg').style.color = '#736f72';
   // document.getElementById('highlightBg').style.color = '#736f72';
 }
 
 function changeColorBack() {
-  document.body.style.backgroundColor = '';
-  // document.getElementById('gitBg').style.color = '';
-  // document.getElementById('linkedBg').style.color = '';
+  // document.body.style.backgroundImage = '';
+  document.getElementById('gitBg').style.color = '';
+  document.getElementById('linkedBg').style.color = '';
   // document.getElementById('aboutmeBg').style.color = '';
   // document.getElementById('highlightBg').style.color = '';
 }
