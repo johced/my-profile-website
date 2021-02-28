527562b816dbe963a67c4eb60315df7d5815c5b9
johced <johan.cederstrand@bahnhof.se>
2021-02-28 17:30:39 +0100

    added js to change cursor

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+window.addEventListener('mousemove', cursor);
+
 window.onload = function () {
   document.getElementById('changeBg').addEventListener('mouseover', changeColorBg);
   document.getElementById('changeBg').addEventListener('mouseout', changeColorBack);
+  mouseCursor = document.querySelector('.cursor');
 };
 
 function changeColorBg() {
@@ -11,4 +14,9 @@ function changeColorBg() {
 function changeColorBack() {
   document.getElementById('gitBg').style.color = '';
   document.getElementById('linkedBg').style.color = '';
+}
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
 }
